fc5651666dffff33ed7b470d00542913da3954ed
terut <terut.dev+github@gmail.com>
2017-03-30 23:10:58 -0700

    Dockernize development for frontend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,7 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: './src/index.jsx',
   output: {
-    path: path.resolve('public'),
+    path: path.resolve('public/assets'),
     filename: 'bundle.js'
   },
   module: {
